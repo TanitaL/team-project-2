a18dc47fe982873fb7803f03225fbd2383460ff7
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-07-24 15:44:07 +0300

    add datapicker

--- src/components/DataPicker/DataPicker.jsx
+++ src/components/DataPicker/DataPicker.jsx
@@ -1,23 +1,23 @@
-// import { Field } from 'formik';
-// import React from 'react'
-// import DatePicker from 'react-datepicker';
-// import DateView from 'react-datepicker';
-// import 'react-datepicker/dist/react-datepicker.css';
+import { Field } from 'formik';
+import React from 'react'
+import DatePicker from 'react-datepicker';
+import DateView from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-// function DataPicker(props) {
-//     const {lable, name, ...rest}=props
-//   return (
-//       <div className="form-control">
-//           <lable htmlFor={name}>{lable}</lable>
-//           <Field name={name}>
-//               {({form, field}) => {
-//                   const { setFildValue } = form 
-//                   const { value } = field
-//                   return <DateView/>
-//               }}
-//           </Field>
-//     </div>
-//   )
-// }
+function DataPicker(props) {
+    const {lable, name, ...rest}=props
+  return (
+      <div className="form-control">
+          <lable htmlFor={name}>{lable}</lable>
+          <Field name={name}>
+              {({form, field}) => {
+                  const { setFildValue } = form 
+                  const { value } = field
+                  return <DateView/>
+              }}
+          </Field>
+    </div>
+  )
+}
 
-// export default DataPicker
+export default DataPicker
