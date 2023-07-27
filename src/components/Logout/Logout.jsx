a961113645adab2e1cf6f407c81bbd92deeb0744
Tetiana Lomiichuk <lomitatiana@gmail.com>
2023-07-27 16:53:35 +0200

    update Header and ModalApproveAction

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -2,16 +2,19 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { austOperationThunk } from 'redux/auth/thunks';
 import ModalApproveAction from 'components/Modals/ModalApproveAction/ModalApproveAction';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import LogoutBtn from 'components/Buttons/LogoutBtn/LogoutBtn';
 import { ReactComponent as LogoutSvg } from 'assets/svg/logout-opt.svg';
 
 const Logout = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSuccess = () => {
     dispatch(austOperationThunk({ endpoint: 'logout' }));
+
+    navigate('/');
   };
 
   const handleCancel = () => {
--- src/components/Navigation/AuthNav/AuthNav.jsx
+++ src/components/Navigation/AuthNav/AuthNav.jsx
@@ -2,27 +2,15 @@ import React from 'react';
 import RegisterBtn from '../../Buttons/RegisterBtn/RegisterBtn';
 import LoginBtn from '../../Buttons/LoginBtn/LoginBtn';
 import css from './AuthNav.module.css';
-
+// import { NavLink } from 'react-router-dom';
 
 const AuthNav = () => {
   return (
     <nav className={css.authNav}>
-        <LoginBtn />
-        <RegisterBtn />
+      <LoginBtn />
+      <RegisterBtn />
     </nav>
   );
 };
 
 export default AuthNav;
-
-
-
-    // <nav className={css.authNav}>
-    //   <NavLink key={'login'} to={'login'} className={css.link}>
-    //     <LoginBtn closeBurgerMenu={() => setMenuOpen(false)} />
-    //   </NavLink>
-
-    //   <NavLink key={'register'} to={'register'} className={css.link}>
-    //     <RegisterBtn closeBurgerMenu={() => setMenuOpen(false)} />
-    //   </NavLink>
-    // </nav>;
