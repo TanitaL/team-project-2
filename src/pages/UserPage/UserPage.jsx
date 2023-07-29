56ff4a5a0fb3ec4f02807afc01eef28dc8e1e08c
Anna <a.zaliubovska@gmail.com>
2023-07-29 18:40:42 +0300

    add chages

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -2,33 +2,41 @@ import React from 'react';
 import Container from 'components/Container/Container';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-// import LogoutBtn from 'components/Buttons/LogoutBtn/LogoutBtn';
+import { ReactComponent as LogoutSvg } from '../UserPage/avatar/logout.svg';
+
+import { BsPlusLg } from 'react-icons/bs';
 import MyPetsList from '../../components/Cards/MyPets/MyPetsList/MyPetsList';
 import css from '../UserPage/UserPage.module.css';
+import { useRef } from 'react';
 
-const validate = Yup.object({
-  avatar: Yup.mixed()
-    .required('Edit poto')
-    .test(
-      'fileSize',
-      'Photo must be up to 3MB',
-      value => value && value.size <= 3000000
-    ),
-  name: Yup.string().required('Name is required'),
-  email: Yup.string()
-    .required('Email is required')
-    .email('Invalid email address'),
-  birthday: Yup.string().required('Birthday is required'),
-  phone: Yup.string()
-    .required('Phone is required')
+import AvatarUpload from './AvatarUpload/AvatarUpload';
+
+  const validate = Yup.object({
+    avatar: Yup.mixed()
+      .required('Edit poto')
+      .test(
+        'fileSize',
+        'Photo must be up to 3MB',
+        value => value && value.size <= 3000000
+      ),
+    name: Yup.string().required('Name is required'),
+    email: Yup.string()
+      .required('Email is required')
+      .email('Invalid email address'),
+    birthday: Yup.string().required('Birthday is required'),
+    phone: Yup.string()
+      .required('Phone is required')
     .matches(/^\+380\d{9}$/, 'Phone number must be in theformat +380XXXXXXXXX'),
-  city: Yup.string().required('City is required'),
-});
+    city: Yup.string().required('City is required'),
+  });
 
 const UserPage = () => {
   const onSubmit = values => {
     console.log(values);
   };
+  const fileRef = useRef(null);
+
+  
 
   return (
     <Container>
@@ -37,7 +45,7 @@ const UserPage = () => {
           <h2 className={css.title}> My information:</h2>
           <Formik
             initialValues={{
-              avatar: '',
+              avatar: null,
               name: '',
               email: '',
               birthday: '',
@@ -47,143 +55,149 @@ const UserPage = () => {
             validationSchema={validate}
             onSubmit={onSubmit}
           >
-            {({ setFieldValue }) => (
+            {({ values, setFieldValue }) => (
               <Form className={css.section}>
                 <div className={css.avatar}>
-                  <picture>
-                    <img
-                      src="/avatar/Photodefault.jpg"
-                      alt=""
-                      className={css.img}
-                    />
-                  </picture>
-
-                  {<label htmlFor="photo"></label>}
-                  {
-                    <input
-                      type="file"
-                      name="photo"
-                      id="photo"
-                      accept="image/*"
-                      onChange={event => {
-                        setFieldValue('photo', event.currentTarget.files[0]);
-                      }}
-                    />
-                  }
+                <input
+                    ref={fileRef}
+                    hidden
+                    type="file"
+                    name="file"
+                    onChange={event => {
+                      setFieldValue('file', event.target.files[0]);
+                    }}
+                  />
+                  <button
+                  className={css.img}
+                  type="button"
+                  onClick={() => {
+                    fileRef.current.click();
+                  }}
+                  >
+                    {!values.file && <BsPlusLg/>}
+                    {values.file && (
+                      <>
+                      <AvatarUpload
+                      file={values.file}
+                      />
+                      </>
+                    )}
+                  </button>
                   <ErrorMessage name="photo" component="div" />
                 </div>
                 <div className={css.profileInput}>
-                  <div className={css.item}>
-                    <div className={css.flexContainer}>
-                      <label htmlFor="name" className={css.label}>
-                        Name:{' '}
-                      </label>
-                      <Field
-                        type="text"
-                        name="name"
-                        id="name"
-                        className={css.input}
-                        placeholder="Enter your name"
-                      />
-                    </div>
-                    <ErrorMessage
-                      name="name"
-                      component="div"
-                      className={css.ErrorMessage}
-                    />
+                <div className={css.item}>
+                  <div className={css.flexContainer}>
+                  <label htmlFor="name" className={css.label}>
+                    Name:{' '}
+                  </label>
+                  <Field
+                    type="text"
+                    name="name"
+                    id="name"
+                    className={css.input}
+                    placeholder="Enter your name"
+                  />
                   </div>
-
-                  <div className={css.item}>
-                    <div className={css.flexContainer}>
-                      <label htmlFor="email" className={css.label}>
-                        Email:{' '}
-                      </label>
-                      <Field
-                        type="email"
-                        name="email"
-                        id="email"
-                        className={css.input}
-                        placeholder="youremail@gmail.com"
-                      />
-                    </div>
-                    <ErrorMessage
-                      name="email"
-                      component="div"
-                      className={css.ErrorMessage}
-                    />
-                  </div>
-
-                  <div className={css.item}>
-                    <div className={css.flexContainer}>
-                      <label htmlFor="birthday" className={css.label}>
-                        Birthday:{' '}
-                      </label>
-                      <Field
-                        type="date"
-                        name="birthday"
-                        id="birthday"
-                        className={css.input}
-                        placeholder="00.00.0000"
-                      />
-                    </div>
-                    <ErrorMessage
-                      name="birthday"
-                      component="div"
-                      className={css.ErrorMessage}
-                    />
-                  </div>
-
-                  <div className={css.item}>
-                    <div className={css.flexContainer}>
-                      <label htmlFor="phone" className={css.label}>
-                        Phone:{' '}
-                      </label>
-                      <Field
-                        type="tel"
-                        name="phone"
-                        id="phone"
-                        className={css.input}
-                        placeholder="+380000000000"
-                      />
-                    </div>
-                    <ErrorMessage
-                      name="phone"
-                      component="div"
-                      className={css.ErrorMessage}
-                    />
-                  </div>
-
-                  <div className={css.item}>
-                    <div className={css.flexContainer}>
-                      <label htmlFor="city" className={css.label}>
-                        City:{' '}
-                      </label>
-                      <Field
-                        type="text"
-                        name="city"
-                        id="city"
-                        className={css.input}
-                        placeholder="Kyiv"
-                      />
-                    </div>
-                    <ErrorMessage
-                      name="city"
-                      component="div"
-                      className={css.ErrorMessage}
-                    />
-                  </div>
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className={css.ErrorMessage}
+                  />
                 </div>
 
-                <button src="" type="submit" className={css.button}>
+                <div className={css.item}>
+                <div className={css.flexContainer}>
+                  <label htmlFor="email" className={css.label}>
+                    Email:{' '}
+                  </label>
+                  <Field
+                    type="email"
+                    name="email"
+                    id="email"
+                    className={css.input}
+                    placeholder="youremail@gmail.com"
+                  />
+                  </div>
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className={css.ErrorMessage}
+                  />
+                </div>
+
+                <div className={css.item}>
+                <div className={css.flexContainer}>
+                  <label htmlFor="birthday" className={css.label}>
+                    Birthday:{' '}
+                  </label>
+                  <Field
+                    type="date"
+                    name="birthday"
+                    id="birthday"
+                    className={css.input}
+                    placeholder="00.00.0000"
+                  />
+                  </div>
+                  <ErrorMessage
+                    name="birthday"
+                    component="div"
+                    className={css.ErrorMessage}
+                  />
+                </div>
+
+                <div className={css.item}>
+                <div className={css.flexContainer}>
+                  <label htmlFor="phone" className={css.label}>
+                    Phone:{' '}
+                  </label>
+                  <Field
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    className={css.input}
+                    placeholder="+380000000000"
+                  />
+                  </div>
+                  <ErrorMessage
+                    name="phone"
+                    component="div"
+                    className={css.ErrorMessage}
+                  />
+                </div>
+
+                <div className={css.item}>
+                <div className={css.flexContainer}>
+                  <label htmlFor="city" className={css.label}>
+                    City:{' '}
+                  </label>
+                  <Field
+                    type="text"
+                    name="city"
+                    id="city"
+                    className={css.input}
+                    placeholder="Kyiv"
+                  />
+                  </div>
+                  <ErrorMessage
+                    name="city"
+                    component="div"
+                    className={css.ErrorMessage}
+                  />
+                </div>
+                </div>
+
+                <button src="" type="button" className={css.button} >
+                <LogoutSvg className={css.buttonSvg}/>
                   Log out
                 </button>
               </Form>
             )}
           </Formik>
         </div>
-        <div>
-          <h2 className={css.title}>My pets:</h2>
-          <MyPetsList />
+        <div className={css.conteinerTitle}>
+          <h2 className={css.title}> My pets:</h2>
+          <MyPetsList/>
         </div>
       </div>
     </Container>
