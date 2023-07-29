53e0df5c84b6017940ebff7c07021ceaa92cacde
Artem Shnurko <shnurko.artyom@gmail.com>
2023-07-29 16:23:45 +0200

    feat(pagination): favicon and pagination added

--- src/components/Cards/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/Cards/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import CategoryItem from './NoticesCategoriesItem/NoticesCategoriesItem';
 import css from './NoticesCategoriesList.module.css';
-
+import { nanoid } from '@reduxjs/toolkit';
 const CategoryList = ({ data }) => {
   return (
     <ul className={css.list}>
-      {data.map(({ _id, title, text, date, file, location, name, sex }) => (
-        <CategoryItem
-          key={_id}
-          _id={_id}
-          title={title}
-          text={text}
-          date={date}
-          file={file}
-          location={location}
-          name={name}
-          sex={sex}
-        />
-      ))}
+      {data.map(
+        ({ _id, title, text, date, file, location, name, sex }, index) => (
+          <CategoryItem
+            key={nanoid(5)}
+            _id={_id}
+            title={title}
+            text={text}
+            date={date}
+            file={file}
+            location={location}
+            name={name}
+            sex={sex}
+          />
+        )
+      )}
     </ul>
   );
 };
