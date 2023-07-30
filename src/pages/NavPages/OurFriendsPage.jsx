9b1ef130dc833bcd0a46f008f98329107824b216
Tetiana Lomiichuk <lomitatiana@gmail.com>
2023-07-31 01:56:26 +0200

    add path to container

--- src/pages/NavPages/OurFriendsPage.jsx
+++ src/pages/NavPages/OurFriendsPage.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import Container from 'components/Container/Container';
+import Container from 'components/Container/Container/Container';
 import { OurFriend } from 'components/OurFriendList/ourFriendList';
-
-
 
 const OurFriendsPage = () => {
   return (
     <Container>
-      
       <OurFriend></OurFriend>
     </Container>
   );
