55ca5e28e38753d4e79697166ab1ac64ba8ac423
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-07-31 17:26:15 +0300

    fix style

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,4 +2,4 @@ export const authSelector = state => state.auth.token;
 export const userSelector = state => state.auth.user;
 export const errorSelector = state => state.auth.error;
 export const favoritesSelector = state => state.auth.user?.favorites;
-export const userIdSelector = state => state.auth.user.id;
+export const userIdSelector = state => state.auth.user?.id;
