c248dba55ae542e88338368074df61d8bb144744
Oleksandr_Moroz <110786733+Kontrabas888@users.noreply.github.com>
2023-07-31 18:38:23 +0300

    cards-m-6

--- src/components/Cards/News/NewsList/NewsList.jsx
+++ src/components/Cards/News/NewsList/NewsList.jsx
@@ -1,9 +1,10 @@
 import NewsItems from './NewsItems/NewsItems';
 import css from './NewsList.module.css';
+import containerCss from '../../../Container/Container/Container.module.css';
 
 const NewsList = ({ news }) => {
   return (
-    <ul className={css.list}>
+    <ul className={`${css.list} ${containerCss.containerMain}`}>
       {news.map(({ url, title, body, date, image }) => (
         <NewsItems
           key={url}
