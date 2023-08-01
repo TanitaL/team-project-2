0abc9e8925c3c66270d7aa42692947a323253d3a
Tetiana Lomiichuk <lomitatiana@gmail.com>
2023-08-01 20:03:19 +0200

    update

--- src/components/Container/BgContainer/BgContainer.jsx
+++ src/components/Container/BgContainer/BgContainer.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
 import css from './BgContainer.module.css';
 
 const BgContainer = ({ children }) => {
   return (
     <>
       <div className={css.bgContainer}>{children}</div>
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
     </>
   );
 };
