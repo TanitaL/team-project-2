fe1e6ef5fb59723f866fbb82dc42f0f0a690e13d
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-08-01 16:29:05 +0300

    add pag

--- src/pages/NavPages/NoticesPage.jsx
+++ src/pages/NavPages/NoticesPage.jsx
@@ -1,98 +1,120 @@
 import React, { useState, useEffect } from 'react';
 import CategoryList from '../../components/Cards/Notices/NoticesCategoriesList/NoticesCategoriesList';
 import css from '../../components/Cards/Notices/NoticesCategoriesList/NoticesCategoriesItem/NoticesCategoriesItem.module.css';
-
 import SearchComponent from '../../components/SearchComponent/SearchComponent';
 import NoticesCategoriesNav from '../../components/NoticesCategoriesNav/NoticesCategoriesNav';
 import NoticesFilters from 'components/NoticesFilters/NoticesFilters';
 import AddPetButton from 'components/AddPetButton/AddPetButton';
-import Pagination from 'components/Pagination/Pagination';
-import { getPets, getIsLoading } from 'redux/pets/selectors';
-import {  useSelector } from 'react-redux';
-import Loader from 'components/Loader/Loader';
+import PaginationNews from 'components/Pagination/PaginationNews';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getPets,
+  getTotalResult,
+  getIsLoading,
+  getError,
+} from 'redux/pets/selectors';
+import { fetchPets } from 'redux/pets/operations';
+import LoaderPet from 'components/LoaderPet/LoaderPet';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
+let initialQueryObj = {
+  page: 1,
+  limit: 12,
+  category: '',
+  sex: '',
+  date: '',
+  query: '',
+};
 
 const NoticesPage = () => {
+  const dispatch = useDispatch();
   const pets = useSelector(getPets);
   const isLoading = useSelector(getIsLoading);
+  const totalResult = useSelector(getTotalResult);
+  const error = useSelector(getError);
+  const [pages, setPages] = useState(0);
+  const [queryObj, setQueryObj] = useState(initialQueryObj);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const itemsPerPage = 10;
-  const [query, setQuery] = useState('');
+  useEffect(() => {
+    let params = { limit: queryObj?.limit, page: queryObj?.page };
 
-  const visiblePets = pets.filter(notice =>
-    notice.title.toLowerCase().includes(query.toLowerCase())
-  );
+    for (const key in queryObj) {
+      if (queryObj[key] === '') continue;
+      params = { ...params, [key]: queryObj[key] };
+    }
+    dispatch(fetchPets(params));
+  }, [queryObj, dispatch]);
 
-  const handleSearch = async searchTerm => {
-    const trimedQuery = searchTerm.trim();
-    if (trimedQuery) {
-      setQuery(trimedQuery);
+  useEffect(() => {
+    const pages = Math.ceil(totalResult / queryObj.limit);
+    setPages(pages);
+  }, [totalResult, queryObj.limit]);
+
+  const handleSetPage = () => {
+    setQueryObj(prevState => ({ ...prevState, page: 1 }));
+  };
+
+  const handleSetCategory = category => {
+    setQueryObj(prevState => ({ ...prevState, category }));
+  };
+
+  const handleSetSex = sex => {
+    setQueryObj(prevState => ({ ...prevState, sex }));
+  };
+
+  const handleSetDate = date => {
+    setQueryObj(prevState => ({ ...prevState, date }));
+  };
+
+  const handleSearch = searchTerm => {
+    if (searchTerm.trim() !== '') {
+      handleSetPage();
+      const correctedSearch = searchTerm.toLowerCase();
+      setQueryObj({ query: correctedSearch });
+    } else {
+      Notify.warning('Please, Enter the correct data for the search', {
+        timeout: 3000,
+        position: 'center-top',
+      });
     }
   };
 
-  useEffect(() => {
-    const pages = Math.ceil(pets.length / itemsPerPage);
-    setTotalPages(pages);
-  }, [pets.length]);
-
   const handlePageChange = pageNumber => {
-    setCurrentPage(pageNumber);
+    setQueryObj(prevState => ({ ...prevState, page: pageNumber }));
+    console.log('queryObj-->', queryObj);
   };
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = visiblePets.slice(indexOfFirstItem, indexOfLastItem);
-
-  // if (isLoading) {
-  //   return (
-  //     <div
-  //       style={{
-  //         display: 'flex',
-  //         justifyContent: 'center',
-  //         alignItems: 'center',
-  //         height: '100vh',
-  //       }}
-  //     >
-  //       <div className="spinner-border text-primary" role="status">
-  //         <span className="visually-hidden">Loading...</span>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
-  // if (error === '404') {
-  //   return (
-  //     <div>
-  //       <img src="https://http.cat/407" alt="Error 404" />
-  //       <p>Oops! Something went wrong.</p>
-  //     </div>
-  //   );
-  // }
+  if (error === '404') {
+    return (
+      <div>
+        <img src="https://http.cat/407" alt="Error 404" />
+        <p>Oops! Something went wrong.</p>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h1 className={css.textNoticesPage}>Find your favorite pet</h1>
-      <div className={css.container} >
-      <SearchComponent onSearch={handleSearch} />
+      <SearchComponent
+        onFilterSex={handleSetSex}
+        onFilterDate={handleSetDate}
+      />
       <div className={css.categoryFilterWrapper}>
-        <NoticesCategoriesNav />
+        <NoticesCategoriesNav onChangeCategory={handleSetCategory} />
         <div className={css.noticeFilter}>
           <NoticesFilters onFilter={handleSearch} />
           <AddPetButton />
-          </div>
         </div>
-        </div>
-      {isLoading && <Loader />}
-      <CategoryList data={currentItems} />
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
       </div>
+      {isLoading && <LoaderPet />}
+      <CategoryList data={pets} />
+      <PaginationNews
+        currentPage={queryObj.page}
+        totalPages={pages}
+        handlePaginationChange={handlePageChange}
+      />
+    </div>
   );
 };
 
