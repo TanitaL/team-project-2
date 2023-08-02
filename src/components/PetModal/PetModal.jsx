0c1b542848c8c2226cd435fcec80e7c0dc7ea5c6
unknown <myron.melnyk.kn.2021@lpnu.ua>
2023-08-02 21:44:57 +0300

    commit

--- src/components/PetModal/PetModal.jsx
+++ src/components/PetModal/PetModal.jsx
@@ -100,113 +100,111 @@ const PetModal = ({ id, onClose, isFavorite, addToFavotire }) => {
     return createPortal(
       <>
         <div className={css.backdrop} onClick={handleOnBackdropClose}>
-          <div className={css.container}>
-            <div className={css.box}>
-              <div className={css.upperBox}>
-                <div className={css.thumb}>
-                  <ImageLoader
-                    src={file}
-                    className={css.image}
-                    alt="Information about pet"
-                  />
-                  <p className={css.category}>{transformCategory(category)}</p>
-                </div>
-                <div className={css.afterImage}>
-                  {title && <h2 className={css.title}>{title}</h2>}
-                  <ul className={css.list}>
-                    <li>
-                      <p className={css.field}>
-                        Name: <span className={css.value}>{name}</span>
-                      </p>
-                    </li>
-
-                    {date && (
-                      <li>
-                        <p className={css.field}>
-                          Birthday: <span className={css.value}>{date}</span>
-                        </p>
-                      </li>
-                    )}
-
-                    <li>
-                      <p className={css.field}>
-                        Type: <span className={css.value}>{type}</span>
-                      </p>
-                    </li>
-
-                    {location && (
-                      <li>
-                        <p className={css.field}>
-                          Place:<span className={css.value}>{location}</span>
-                        </p>
-                      </li>
-                    )}
-
-                    {sex && (
-                      <li>
-                        <p className={css.field}>
-                          The sex: <span className={css.value}>{sex}</span>
-                        </p>
-                      </li>
-                    )}
-
-                    {price && (
-                      <li>
-                        <p className={css.field}>
-                          Price: <span className={css.value}>{price}</span>
-                        </p>
-                      </li>
-                    )}
-
-                    <li>
-                      <p className={css.field}>
-                        Email:
-                        <span className={css.goldValue}>{owner.email}</span>
-                      </p>
-                    </li>
-
-                    {owner.phone && (
-                      <li>
-                        <p className={css.field}>
-                          Phone:
-                          <span className={css.goldValue}>{owner.phone}</span>
-                        </p>
-                      </li>
-                    )}
-                  </ul>
-                </div>
+          <div className={css.modal}>
+            <div className={css.upperBox}>
+              <div className={css.thumb}>
+                <ImageLoader
+                  src={file}
+                  className={css.image}
+                  alt="Information about pet"
+                />
+                <p className={css.category}>{transformCategory(category)}</p>
               </div>
-              <div className={css.lowerBox}>
-                {comments && (
-                  <p className={css.lowerBoxComment}>Comments: {comments}</p>
-                )}
+              <div className={css.afterImage}>
+                {title && <h2 className={css.title}>{title}</h2>}
+                <ul className={css.list}>
+                  <li>
+                    <p className={css.field}>
+                      Name: <span className={css.value}>{name}</span>
+                    </p>
+                  </li>
 
-                <div className={css.lowerBtns}>
-                  <button
-                    className={css.addToFavotiteBtn}
-                    onClick={addToFavotire}
-                  >
-                    {isFavorite ? (
-                      <>
-                        <p>Delete from</p>
-                        <HeartSvgFilled />
-                      </>
-                    ) : (
-                      <>
-                        <p>Add to</p>
-                        <HeartSvg />
-                      </>
-                    )}
-                  </button>
-                  <a href={`mailto:${owner.email}`} className={css.contactBtn}>
-                    Contact
-                  </a>
-                </div>
+                  {date && (
+                    <li>
+                      <p className={css.field}>
+                        Birthday: <span className={css.value}>{date}</span>
+                      </p>
+                    </li>
+                  )}
+
+                  <li>
+                    <p className={css.field}>
+                      Type: <span className={css.value}>{type}</span>
+                    </p>
+                  </li>
+
+                  {location && (
+                    <li>
+                      <p className={css.field}>
+                        Place:<span className={css.value}>{location}</span>
+                      </p>
+                    </li>
+                  )}
+
+                  {sex && (
+                    <li>
+                      <p className={css.field}>
+                        The sex: <span className={css.value}>{sex}</span>
+                      </p>
+                    </li>
+                  )}
+
+                  {price && (
+                    <li>
+                      <p className={css.field}>
+                        Price: <span className={css.value}>{price}</span>
+                      </p>
+                    </li>
+                  )}
+
+                  <li>
+                    <p className={css.field}>
+                      Email:
+                      <span className={css.goldValue}>{owner.email}</span>
+                    </p>
+                  </li>
+
+                  {owner.phone && (
+                    <li>
+                      <p className={css.field}>
+                        Phone:
+                        <span className={css.goldValue}>{owner.phone}</span>
+                      </p>
+                    </li>
+                  )}
+                </ul>
               </div>
-              <button className={css.closeBtn} onClick={onClose}>
-                <CloseSvg />
-              </button>
             </div>
+            <div className={css.lowerBox}>
+              {comments && (
+                <p className={css.lowerBoxComment}>Comments: {comments}</p>
+              )}
+
+              <div className={css.lowerBtns}>
+                <button
+                  className={css.addToFavotiteBtn}
+                  onClick={addToFavotire}
+                >
+                  {isFavorite ? (
+                    <>
+                      <p>Delete from</p>
+                      <HeartSvgFilled />
+                    </>
+                  ) : (
+                    <>
+                      <p>Add to</p>
+                      <HeartSvg />
+                    </>
+                  )}
+                </button>
+                <a href={`mailto:${owner.email}`} className={css.contactBtn}>
+                  Contact
+                </a>
+              </div>
+            </div>
+            <button className={css.closeBtn} onClick={onClose}>
+              <CloseSvg />
+            </button>
           </div>
         </div>
       </>,
